b1bfb88ca390f64eb6ce1862d0c45c791679022c
Julia Maddock <jm063116@ohio.edu>
2019-02-13 18:19:35 -0500

    Add files via upload

---
+++ Maddock_Hw_4.py
@@ -0,0 +1,8 @@
+#Name: Julia Maddock
+#Date(Y/M/D): 19 02 13
+#Course: CS 3560
+#Ohio University
+#Instructor: Chad Mourning
+#HW Assignment 4
+
+print (sum([i for i in range(1000) if i % 3 == 0 or i % 5 == 0]))
