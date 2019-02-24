3c60be43a7dc64d77c1b987be45106593a1f3f7f
Julia Maddock <jm063116@ohio.edu>
2019-02-24 17:13:16 -0500

    Add files via upload

---
+++ call(2.0).sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#it worked for me, b/c I typed this in the command prompt, but I don't know if the change is permanent or if it would need to be done again when used in a separate terminal. So, I am resubmitting this file with the command built into the script just in case the first file doesn't work.
+
+chmod+x call.sh 
+
+python counter.py *.txt
+
+
+
+
